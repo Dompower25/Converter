6e42e6907ba79c80d2b6be110eac603f7fbf35e5
Dompower25 <89357747+Dompower25@users.noreply.github.com>
2022-03-30 09:34:41 +0300

    change 28.03.2022
    Не определяет rate

--- js/script.js
+++ js/script.js
@@ -1,95 +1,156 @@
 /*
  * https://www.nbrb.by/apihelp/exrates
  */
-const buttonAdd = document.querySelector('.add-convertor__box');
+const buttonAdd = document.querySelector(".add-convertor__box");
+const inputByn = document.querySelector("#BYN");
+const inputUsd = document.querySelector("#USD");
+const inputEur = document.querySelector("#EUR");
+const inputAmd = document.querySelector("#AMD");
+const inputAud = document.querySelector("#AUD");
+const inputBgn = document.querySelector("#BGN");
+const inputCad = document.querySelector("#CAD");
+const inputChf = document.querySelector("#CHF");
+const inputCny = document.querySelector("#CNY");
+const inputCzk = document.querySelector("#CZK");
+const inputDkk = document.querySelector("#DKK");
+const inputGbp = document.querySelector("#GBP");
+const inputIrr = document.querySelector("#IRR");
+const inputIsk = document.querySelector("#ISK");
+const inputJpy = document.querySelector("#JPY");
+const inputKgs = document.querySelector("#KGS");
+const inputKwd = document.querySelector("#KWD");
+const inputKzt = document.querySelector("#KZT");
+const inputMdl = document.querySelector("#MDL");
+const inputNok = document.querySelector("#NOK");
+const inputNzd = document.querySelector("#NZD");
+const inputPln = document.querySelector("#PLN");
+const inputRub = document.querySelector("#RUB");
+const inputSek = document.querySelector("#SEK");
+const inputSgd = document.querySelector("#SGD");
+const inputTry = document.querySelector("#TRY");
+const inputUah = document.querySelector("#UAH");
+const inputXdr = document.querySelector("#XDR");
 
-const inputUsb = document.querySelector('#USD');
-const inputByn = document.querySelector('#BYN');
-const inputEur = document.querySelector('#EUR');
+const availableInputs = [
+  inputUsd,
+  inputByn,
+  inputEur,
+  inputAmd,
+  inputAud,
+  inputBgn,
+  inputCad,
+  inputChf,
+  inputCny,
+  inputCzk,
+  inputDkk,
+  inputGbp,
+  inputIrr,
+  inputIsk,
+  inputJpy,
+  inputKgs,
+  inputKwd,
+  inputKzt,
+  inputMdl,
+  inputNok,
+  inputNzd,
+  inputPln,
+  inputRub,
+  inputSek,
+  inputSgd,
+  inputTry,
+  inputUah,
+  inputXdr,
+];
 
 const url = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
+const inputAll = document.querySelector(".void__box");
+const inputCollection = document.querySelector(".convertor__box");
+const containerToAdd = document.querySelector(".add__container");
 
-const inputAll = document.querySelector('.void__box');
-const inputCollection = document.querySelector('.convertor__box');
+function toByn(currency, { rate, scale }) {
+  return (currency * (rate * scale)).toFixed(2);
+}
+
+function fromByn(byn, { rate, scale }) {
+  return (byn / (rate * scale)).toFixed(2);
+}
+
+function recalcAll(byn, availableInputs, convertionMapBynVal) {
+  availableInputs.forEach((input) => {
+    const currencyName = input?.id;
+    const currencyToByn = convertionMapBynVal[currencyName];
+    input.value = fromByn(byn, currencyToByn);
+  });
+}
 
 async function getCours() {
-   try {
-      const response = await fetch(url);
-      const data = await response.json();
-		
-		const currMap = {} // список объектов
-		
-			
-		const convertionMapBynVal = {}				
-		for (let i = 0; i < data.length; i++) {	
-			
-			const e = {};		
-			
-			const Elem = data[i];
-			const num = Elem.Cur_OfficialRate;
-			const nameVal = Elem.Cur_Abbreviation;			
-			const curSc = Elem.Cur_Scale;
-			let urlIdText = '#' + `${nameVal}`;
-			const urlInput = document.querySelector(urlIdText)					
-			convertionMapBynVal[nameVal] = e;
+  try {
+    const response = await fetch(url);
+    const officialCurrencies = await response.json();
+    const convertionMapBynVal = {};
 
-			e.NameRate = nameVal;
-			e.Rate = num;			
-			e.CurScale = curSc;
-			e.UrlInpuDocument = urlInput;							
-			
-			
-		}	
-		calculateUsbVal();
-		inputAll.addEventListener('input', targetClick);
-		
-		function targetClick(e) {
-			let inc = e.target.id;
-			switch(inc) {
-				case 'BYN':
-					calculateBynVal()
-					break;		
-				case 'USD':
-					calculateUsbVal()
-					break;
-				case 'EUR':
-					calculateEurVal()
-					break;
-			}			
-					
-		};
-		
-		function calculateBynVal() {
-			inputUsb.value = (inputByn.value / (convertionMapBynVal.USD.Rate*convertionMapBynVal.USD.CurScale)).toFixed(2);
-			inputEur.value = (inputByn.value / (convertionMapBynVal.EUR.Rate*convertionMapBynVal.EUR.CurScale)).toFixed(2);		
-		
-		};
+    officialCurrencies.forEach(
+      ({ Cur_OfficialRate, Cur_Abbreviation, Cur_Scale }) => {
+        convertionMapBynVal[Cur_Abbreviation] = {
+          rate: Cur_OfficialRate,
+          scale: Cur_Scale,
+        };
+      }
+    );
 
-		function calculateUsbVal() {
-			inputByn.value = (inputUsb.value * (convertionMapBynVal.USD.Rate * convertionMapBynVal.USD.CurScale)).toFixed(2);
-			
-			function calculateBynVal() {			
-			inputEur.value = (inputByn.value / (convertionMapBynVal.EUR.Rate*convertionMapBynVal.EUR.CurScale)).toFixed(2);
-				
-		}
-			calculateBynVal()
-		};
+    convertionMapBynVal.BYN = {
+      rate: 1,
+      scale: 1,
+    };
+    console.log(convertionMapBynVal)
+    inputAll.addEventListener("input", (e) => {
+      const targetInput = e.target;
+      const currencyName = targetInput.id;
+      const currencyValue = targetInput.value;
+      const currencyInByn = toByn(
+        currencyValue,
+        convertionMapBynVal[currencyName]
+      );
+      const allInputsExceptCurrent = availableInputs.filter(
+        (input) => input !== targetInput
+      );
+      recalcAll(currencyInByn, allInputsExceptCurrent, convertionMapBynVal);
+    });
 
-		function calculateEurVal() {
-			inputByn.value = (inputEur.value * (convertionMapBynVal.EUR.Rate * convertionMapBynVal.EUR.CurScale)).toFixed(2);
+    buttonAdd.addEventListener("click", addToInput);
+	  function addToInput() {
+		 
+	 }
 
-			function calculateBynVal() {
-			inputUsb.value = (inputByn.value / (convertionMapBynVal.USD.Rate*convertionMapBynVal.USD.CurScale)).toFixed(2);
-		
-		}
-			calculateBynVal()
-		};	
-		
 
-   } catch (error) {
-   	console.log(error)
-   };
-    
-};
 
+	  officialCurrencies.forEach(item =>  {
+
+      const createDivConvertorBox = document.createElement("div");
+		  const createInput = document.createElement("input");
+		  const createDivCurrency = document.createElement("div");
+		  const createSpanCountryFlag = document.createElement("span");
+		  const createSpanValName = document.createElement("span");
+
+		  createDivConvertorBox.className = "convertor__box row";
+		  createInput.type = "number";
+		  createInput.id = item.Cur_Abbreviation;
+		  createInput.className = "form-control";
+		  createDivCurrency.className = "currency row";
+		  createSpanCountryFlag.className = "country-flag flag-usd";
+		  createSpanValName.className = "val-name";
+		  createSpanValName.innerText = item.Cur_Abbreviation;
+
+		  containerToAdd.appendChild(createDivConvertorBox);
+		  createDivConvertorBox.appendChild(createInput);
+		  createDivConvertorBox.appendChild(createDivCurrency);
+		  createDivCurrency.appendChild(createSpanCountryFlag);
+		  createDivCurrency.appendChild(createSpanValName);
+	  	}
+	  );
+		  
+  } catch (error) {
+    console.log(error);
+  }
+}
 getCours();
