bd048cc05c0bece243e2c50ba4e35cafd84c2c74
Dompower25 <89357747+Dompower25@users.noreply.github.com>
2022-03-31 19:50:53 +0300

    change 31.03.22[19:50]
    rate > undefiend

--- js/script.js
+++ js/script.js
@@ -2,65 +2,14 @@
  * https://www.nbrb.by/apihelp/exrates
  */
 const buttonAdd = document.querySelector(".add-convertor__box");
+
 const inputByn = document.querySelector("#BYN");
 const inputUsd = document.querySelector("#USD");
 const inputEur = document.querySelector("#EUR");
-const inputAmd = document.querySelector("#AMD");
-const inputAud = document.querySelector("#AUD");
-const inputBgn = document.querySelector("#BGN");
-const inputCad = document.querySelector("#CAD");
-const inputChf = document.querySelector("#CHF");
-const inputCny = document.querySelector("#CNY");
-const inputCzk = document.querySelector("#CZK");
-const inputDkk = document.querySelector("#DKK");
-const inputGbp = document.querySelector("#GBP");
-const inputIrr = document.querySelector("#IRR");
-const inputIsk = document.querySelector("#ISK");
-const inputJpy = document.querySelector("#JPY");
-const inputKgs = document.querySelector("#KGS");
-const inputKwd = document.querySelector("#KWD");
-const inputKzt = document.querySelector("#KZT");
-const inputMdl = document.querySelector("#MDL");
-const inputNok = document.querySelector("#NOK");
-const inputNzd = document.querySelector("#NZD");
-const inputPln = document.querySelector("#PLN");
-const inputRub = document.querySelector("#RUB");
-const inputSek = document.querySelector("#SEK");
-const inputSgd = document.querySelector("#SGD");
-const inputTry = document.querySelector("#TRY");
-const inputUah = document.querySelector("#UAH");
-const inputXdr = document.querySelector("#XDR");
 
-const availableInputs = [
-  inputUsd,
-  inputByn,
-  inputEur,
-  inputAmd,
-  inputAud,
-  inputBgn,
-  inputCad,
-  inputChf,
-  inputCny,
-  inputCzk,
-  inputDkk,
-  inputGbp,
-  inputIrr,
-  inputIsk,
-  inputJpy,
-  inputKgs,
-  inputKwd,
-  inputKzt,
-  inputMdl,
-  inputNok,
-  inputNzd,
-  inputPln,
-  inputRub,
-  inputSek,
-  inputSgd,
-  inputTry,
-  inputUah,
-  inputXdr,
-];
+const availableInputs = ["USD", "BYN", "EUR"].map((idItem) => {
+	document.createElement('input', { type: 'number', id: idItem, className: 'form-control', value: '' });
+});
 
 const url = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
 const inputAll = document.querySelector(".void__box");
@@ -69,19 +18,19 @@ const containerToAdd = document.querySelector(".add__container");
 
 function toByn(currency, { rate, scale }) {
   return (currency * (rate * scale)).toFixed(2);
-}
+};
 
 function fromByn(byn, { rate, scale }) {
   return (byn / (rate * scale)).toFixed(2);
-}
+};
 
 function recalcAll(byn, availableInputs, convertionMapBynVal) {
   availableInputs.forEach((input) => {
     const currencyName = input?.id;
     const currencyToByn = convertionMapBynVal[currencyName];
     input.value = fromByn(byn, currencyToByn);
-  });
-}
+  })
+};
 
 async function getCours() {
   try {
@@ -102,7 +51,7 @@ async function getCours() {
       rate: 1,
       scale: 1,
     };
-    console.log(convertionMapBynVal)
+  
     inputAll.addEventListener("input", (e) => {
       const targetInput = e.target;
       const currencyName = targetInput.id;
@@ -116,17 +65,9 @@ async function getCours() {
       );
       recalcAll(currencyInByn, allInputsExceptCurrent, convertionMapBynVal);
     });
-
-    buttonAdd.addEventListener("click", addToInput);
-	  function addToInput() {
-		 
-	 }
-
-
-
-	  officialCurrencies.forEach(item =>  {
-
-      const createDivConvertorBox = document.createElement("div");
+	  
+	  officialCurrencies.forEach((item) => {
+		  const createDivConvertorBox = document.createElement("div");
 		  const createInput = document.createElement("input");
 		  const createDivCurrency = document.createElement("div");
 		  const createSpanCountryFlag = document.createElement("span");
@@ -136,6 +77,7 @@ async function getCours() {
 		  createInput.type = "number";
 		  createInput.id = item.Cur_Abbreviation;
 		  createInput.className = "form-control";
+		  createInput.value = "";
 		  createDivCurrency.className = "currency row";
 		  createSpanCountryFlag.className = "country-flag flag-usd";
 		  createSpanValName.className = "val-name";
@@ -146,9 +88,8 @@ async function getCours() {
 		  createDivConvertorBox.appendChild(createDivCurrency);
 		  createDivCurrency.appendChild(createSpanCountryFlag);
 		  createDivCurrency.appendChild(createSpanValName);
-	  	}
-	  );
-		  
+	  });
+	 
   } catch (error) {
     console.log(error);
   }
